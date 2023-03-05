df9d57bfbbf3790a29ba3ff3c5cf56ae29db8053
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2023-03-05 11:49:28 +0900

    Remove unused upload script

--- presentations/upload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-rm -rf jvkersch.github.io
-git clone https://github.com/jvkersch/jvkersch.github.io
-quarto render --output-dir jvkersch.github.io/course/math-0/
-cd jvkersch.github.io && git add course/math-0 && git commit -am "Automatic upload" && git push origin main
